9b0e0cc23d299a4b8c40604a6dce915ac32878fd
equeferrer <emferrer4@gmail.com>
2021-03-29 20:40:55 +0800

    Adds dependent destroy for batch_lesson connection

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -2,6 +2,6 @@ class Batch < ApplicationRecord
   validates :name, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
-  has_many :batch_lessons
+  has_many :batch_lessons, dependent: :destroy
   has_many :lessons, through: :batch_lessons
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,6 +1,6 @@
 class Lesson < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
-  has_many :batch_lessons
+  has_many :batch_lessons, dependent: :destroy
   has_many :batches, through: :batch_lessons
 end
