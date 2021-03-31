43b86b8017d70e9d10c63605c1743a3c1618e202
jeleencubillas <jpclavecilla@gmail.com>
2021-03-31 20:39:19 +0800

    Configures Views and Routes for Students and Admins

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,17 @@ Rails.application.routes.draw do
   get 'batches/edit/:id'  => 'batches#edit',    as: 'batch_edit'
   patch 'batches/:id'     => 'batches#update',  as: 'batch_update'
   delete 'batches/:id'    => 'batches#destroy', as: 'batch_delete'
-  devise_for :users, path: 'users'
-  devise_for :admins, path: 'admins'
+  devise_for :students, path: '',
+    controllers: { 
+      sessions: "student/sessions",
+      registrations: "student/registrations",
+      passwords: "student/passwords"
+    }
+  devise_for :admins, path: 'admin',
+    controllers: { 
+      sessions: "admin/sessions",
+      registrations: "admin/registrations",
+      passwords: "admin/passwords"
+    }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
