a5801683c2d6e355b0f9abfca03354fafea567ba
equeferrer <emferrer4@gmail.com>
2021-04-02 13:49:47 +0800

    Adds Lessons Controller for CRUD

--- spec/requests/lessons_controller_spec.rb
+++ spec/requests/lessons_controller_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe 'LessonsController', type: :request do
 
   describe 'POST /lessons' do
     it 'lets admin create new lesson' do
-      params = { name: 'Test Lesson 2', content: 'Sample content for Test lesson 2' }
+      params = { title: 'Test Lesson 2', content: 'Sample content for Test lesson 2' }
       post lesson_create_path, params: { lesson: params }
       expect(response).to redirect_to(lessons_path)
     end
@@ -46,14 +46,14 @@ RSpec.describe 'LessonsController', type: :request do
 
   describe 'PATCH /lessons/edit/:id' do
     it 'updates edited parameters' do
-      get lessons_update_path(id: lesson.id)
+      get lesson_update_path(id: lesson.id)
       expect(response).to have_http_status(:ok)
     end
   end
 
   describe 'DELETE /lessons/:id' do
     it 'deletes a specific lesson' do
-      delete lessons_delete_path(id: lesson.id)
+      delete lesson_delete_path(id: lesson.id)
       expect { Lesson.find(lesson.id) }.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
