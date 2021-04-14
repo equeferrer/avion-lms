4571388de548c89dbcf7fd9bdf45b177d645c260
equeferrer <emferrer4@gmail.com>
2021-04-14 15:19:33 +0800

    edits before_action for registration

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -3,14 +3,12 @@
 class Admins::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
-  
+
   def create
-    before_action :configure_sign_up_params
     super
   end
 
   def update
-    before_action :configure_account_update_params
     super
   end
 
