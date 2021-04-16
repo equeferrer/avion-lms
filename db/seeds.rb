578dfa39bb220c5ea8a6b16962e3ca0a5953713a
equeferrer <emferrer4@gmail.com>
2021-04-17 03:09:31 +0800

    adds seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,87 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Lesson.create(
+  title: 'Lesson 1 - Git Fundamentals, HTML CSS Exercise',
+  content: 'Description of version control and Git, Basic terminal commands, Quick HTML & CSS Exercise',
+  video_url: 'https://drive.google.com/file/d/14-TSz-hA_scfTKgsJT7QrS8onY3IlVyf/preview',
+  document_url: 'https://docs.avionschool.com/precourse/lessons/lesson-1-git-fundamentals',
+)
+
+Lesson.create(
+  title: 'Lesson 2 - Intro to HTML and CSS',
+  content: 'Introduction to HTML, Basic HTML elements, Introduction to CSS, CSS selectors and the <style> element, Basic CSS styling, Introduction to the Box Model, External Stylesheets and the <link> element',
+  video_url: 'https://drive.google.com/file/d/1WleMoqMd7KvhtPEq-hVallYZoELFbiP-/preview',
+  document_url: 'https://docs.avionschool.com/precourse/lessons/lesson-2-html-and-css',
+)
+
+Lesson.create(
+  title: 'Lesson 3 - Intro to CSS',
+  content: 'Introduction to positioning, Display property, Position property, Using the position property to position elements, Introduction to Flexbox',
+  video_url: 'https://drive.google.com/file/d/1zIWMs2Pe14VlFTVFl60nEUkM1IAQST4u/preview',
+  document_url: 'https://docs.avionschool.com/precourse/lessons/untitled-10',
+)
+
+Lesson.create(
+  title: 'Lesson 4 - Introduction to Javascript',
+  content: 'Introduction to JavaScript, Variables, Strings, Numbers, and Boolean, Math, Introduction to Functions, Control Flow and Comparison Operators, How to run automated tests for homework files',
+  video_url: 'https://drive.google.com/file/d/1635v-nkySUKhP9HlGmvTOQHV9G2p2nka/preview',
+  document_url: 'https://docs.avionschool.com/precourse/lessons/untitled-9',
+)
+
+Lesson.create(
+  title: 'Lesson 5 - Javascript II (Control Flow, Comparison Operators, for Loops)',
+  content: 'Undefined and Null, Comparison Operators (continued), Control Flow (continued), Logical Operators, for Loops',
+  video_url: 'https://drive.google.com/file/d/1HKwWKX3rQ1hBklLBuZrLO_dAsVGlZoV5/preview',
+  document_url: 'https://docs.avionschool.com/precourse/lessons/untitled-8',
+)
+
+Lesson.create(
+  title: 'Lesson 6 - Javascript III (for Loops continued, and Arrays)',
+  content: 'Introduction to arrays, for Loops with arrays',
+  video_url: 'https://drive.google.com/file/d/1WLCiTu9rvuEoN4biwqwRG3GDokiqWJt8/preview',
+  document_url: 'https://docs.avionschool.com/precourse/lessons/untitled-7',
+)
+
+Lesson.create(
+  title: 'Lesson 7 - Javascript IV (Objects)',
+  content: 'Introduction to Objects, Methods, for...in Loops, The this Keyword, Objects in JavaScript',
+  video_url: 'https://drive.google.com/file/d/1mMXOku-xyjQDCXQM8-0i3qMTrVAiujLv/preview',
+  document_url: 'https://docs.avionschool.com/precourse/lessons/untitled-6',
+)
+
+Lesson.create(
+  title: 'Lesson 8 - Javascript V (Classes and Prototypes)',
+  content: 'Classes, Prototypes',
+  video_url: 'https://drive.google.com/file/d/17QmW5Y0IUgZVdwjVLfnbvPI9hFUlW7BD/preview',
+  document_url: 'https://docs.avionschool.com/precourse/lessons/untitled-5',
+)
+
+Lesson.create(
+  title: 'Lesson 9 - Javascript VI (Callbacks and More Array Methods)',
+  content: 'Callbacks, More Array Methods',
+  video_url: 'https://drive.google.com/file/d/1fEH3GXRmfMgYTC_uYA61E9hpr2DyGopj/preview',
+  document_url: 'https://docs.avionschool.com/precourse/lessons/untitled-4',
+)
+
+Lesson.create(
+  title: 'Lesson 10 - Javascript VII (Closure)',
+  content: 'Closure',
+  video_url: 'https://drive.google.com/file/d/1QaE5cTWXb_dQyu_O_uDqplANMja-Wqbl/preview',
+  document_url: 'https://docs.avionschool.com/precourse/lessons/untitled-3',
+)
+
+Lesson.create(
+  title: 'Lesson 11 - Javascript VIII (Recursion)',
+  content: 'Recursion',
+  video_url: 'https://drive.google.com/file/d/1z8knduFPPUWDeXjUyMdzDAdXibQQDGJj/preview',
+  document_url: 'https://docs.avionschool.com/precourse/lessons/untitled-2',
+)
+
+Lesson.create(
+  title: 'Lesson 12 - Document Object Model',
+  content: 'Introduction to the DOM, The script Element, document, document Selectors, Element Methods and Properties, Event Handlers',
+  video_url: 'https://drive.google.com/file/d/1J-3vVBqnO1-RcATa-M_QEUUx_hupnZ5G/preview',
+  document_url: 'https://docs.avionschool.com/precourse/lessons/untitled-1',
+)
