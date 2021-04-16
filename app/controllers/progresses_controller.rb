f40ca8ca036b549606403b6371c8f9fd21f62a7d
equeferrer <emferrer4@gmail.com>
2021-04-16 17:57:08 +0800

    associates students with progress, adds views

--- app/controllers/progresses_controller.rb
+++ app/controllers/progresses_controller.rb
@@ -1,11 +1,44 @@
 class ProgressesController < ApplicationController
-  def new; end
+  before_action :authenticate_student!
 
-  def create; end
+  def new
+    @progress = Progress.new
+  end
 
-  def show; end
+  def create
+    @progress = Progress.new(progress_params)
+    @progress.student_id = current_student.id
+    if @progress.save
+      redirect_to progress_show_path(@progress)
+    else
+      render :new
+    end
+  end
 
-  def edit; end
+  def show
+    @progress = Progress.find(set_progress)
+  end
 
-  def update; end
+  def edit
+    @progress = Progress.find(set_progress)
+  end
+
+  def update
+    @progress = Progress.find(set_progress)
+    if @progress.update(progress_params)
+      redirect_to progress_show_path(@progress)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_progress
+    params[:id]
+  end
+
+  def progress_params
+    params.require(:progress).permit(:username, :repo, :passed, :total, :pending)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_16_082059) do
+ActiveRecord::Schema.define(version: 2021_04_16_091938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2021_04_16_082059) do
     t.text "pending"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "student_id"
   end
 
   create_table "students", force: :cascade do |t|
